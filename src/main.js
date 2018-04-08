29d218090dc6f18b925d8fb3743d210b69952ca2
LeonApache <juancarlos.apache@gmail.com>
2018-04-08 01:55:12 -0500

    react funcionando

--- src/main.js
+++ src/main.js
@@ -1,1 +1,47 @@
-console.log("Hello Universe");
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class MyApp extends Component {
+
+	constructor(){
+		super();
+		this.state = {
+			name:'',
+			lastName: ''
+		};
+	}
+
+	handleName = (e) => {
+		this.setState({
+			name: e.target.value
+		});
+	}
+
+	handleLastName = (e) => {
+		this.setState({
+			lastName: e.target.value
+		});
+	}
+
+	render(){
+		return (
+					<div>
+						<h4>Form</h4>
+						<div>
+							<label>Name</label>
+							<input type='text' onChange= {e => this.handleName(e)}/>
+							<label>Last Name</label>
+							<input type='text' onChange= {e => this.handleLastName(e)}/>
+						</div>
+						<div>
+							{this.state.name}
+							{this.state.lastName}
+						</div>
+					</div>
+
+				);
+	}
+
+}
+
+ReactDOM.render(<MyApp/>, document.getElementById('my-app'));
